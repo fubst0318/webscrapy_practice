9ab3ab56c66db6b8c51141c54e8b95b795cc9438
fubst0318 <fubst0318@126.com>
2018-05-28 07:20:44 +0800

    modify error raiser

--- chapter1/scrapytest.py
+++ chapter1/scrapytest.py
@@ -9,12 +9,12 @@ from bs4 import BeautifulSoup
 def getTitle(url):
     try:
         html = urlopen(url)
-    except HTTPError as e:
+    except HTTPError:
         return None
     try:
         bsObj = BeautifulSoup(html.read())
         title = bsObj.body.h1
-    except AttributeError as e:
+    except AttributeError:
         return None
     return title
 
